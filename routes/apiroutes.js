b9c6747c1e206ef3455db4773a5e0e003f45cbc3
Dameon Davis <dad1977ut@yahoo.com>
2021-05-10 16:02:47 -0600

    updated routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -16,3 +16,43 @@ router.get("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
 });
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.get(`/api/workouts/range`, (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .limit(7)
+    .sort({ _id: 1 })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+module.exports = router;
